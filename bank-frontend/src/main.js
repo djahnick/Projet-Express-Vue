eb51b8b663ec64640d68e204c1f1c0e364c51984
djahnick <djagene@gmail.com>
2023-11-07 11:06:55 +0100

    MAJ ARCHI

---
+++ bank-frontend/src/main.js
@@ -0,0 +1,13 @@
+// src/main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Assurez-vous que le chemin est correct
+
+import './index.css'; // Assurez-vous que cette ligne est ajoutÃ©e
+
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
